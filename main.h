0f260e15a0ce4c0d5eaa600bc9dedac368039e3e
Hichiki05 <safaeelbazofficial@gmail.com>
2023-10-16 06:03:01 -0700

    ll

--- main.h
+++ main.h
@@ -5,10 +5,15 @@
 #include <unistd.h>
 
 #define UNUSED(x) (void)(x)
-#define BUFF_size 1024
+#define BUFF_SIZE 1024
 
 
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,va_list list, char buffer[], int flags, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *foirmat, int *i, va_list list);
+int get_size(const char *format, int *i);
 
 
 #endif /* MAIN_H */
