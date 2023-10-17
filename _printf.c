553b9aedcff39c66dd312db9346874b02fc4cef7
Hichiki05 <safaeelbazofficial@gmail.com>
2023-10-17 09:54:46 -0700

    jj

--- _printf.c
+++ _printf.c
@@ -8,13 +8,9 @@ int _printf(const char *format, ...)
 {
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin},
-		{"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+		{"%%", printf_37}
 	};
+
 	va_list args;
 	int i = 0, j, len = 0;
 
--- main.h
+++ main.h
@@ -20,23 +20,10 @@ typedef struct format
 	int (*f)();
 } convert_match;
 
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_hex(va_list val);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
 int _putchar(char c);
-int _printf(const char *format, ...);
-
+int printf_string(va_list val);
+int printf_char(va_list val);
+int printf_37(void);
+int _printf(const char * const format, ...);
+int _strlen(char *s);
 #endif 
